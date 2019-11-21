936249243c6b17af919512a07c29d65a05e82ce6
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-11-21 15:27:51 -0500

    SE AGREGO LOS PROGRAMAS DE RODRIGUES FIESTAS

---
+++ RodriguesFiestas.py
@@ -0,0 +1,384 @@
+#Programa21- Calcular el promedio de total pasivo
+#Declaración
+t_pasivo2009=0.0
+t_pasivo2010=0.0
+t_pasivo2011=0.0
+promedioT=0.0
+
+#Input
+t_pasivo2009=24.806
+t_pasivo2010=26.027
+t_pasivo2011=26.501
+promedioT=0.0
+
+#Processing
+promedioT=(t_pasivo2009+t_pasivo2010+t_pasivo2011)/3
+
+#Output
+print("Promedio de total pasivo: ",promedioT)
+
+#Programa22- Calcular el promedio total de capital contable
+#Declaración
+t_cap_contable2009=0.0
+t_cap_contable2010=0.0
+t_cap_contable2011=0.0
+promedioC=0.0
+
+#Input
+t_cap_contable2009=19.677
+t_cap_contable2010=14.821
+t_cap_contable2011=12.300
+promedioC=0.0
+
+
+#Processing
+promedioC=(t_cap_contable2009+t_cap_contable2010+t_cap_contable2011)/3
+
+#Output
+print("Promedio total de capital contable: ",promedioC)
+
+#Programa23- Calcular el total activos
+#Declaración
+t_activos2006=0.0
+t_activos2007=0.0
+t_activos2005=0.0
+total=0.0
+
+#Input
+t_activos2006=29.972
+t_activos2007=49.662
+t_activos2005=26.763
+Total=0.0
+
+#Processing
+Total=(t_activos2006+t_activos2007+t_activos2005)
+
+#Output
+print("Total: ",Total)
+
+#Programa24- Calcular el promedio de total de la participacion controladora
+#Declaración
+t_participacion_controladora2009=0.0
+t_participacion_controladora2010=0.0
+t_participacion_controladora2011=0.0
+promedioK=0.0
+
+#Input
+t_participacion_controladora2009=16.339
+t_participacion_controladora2010=13.248
+t_participacion_controladora2011=11.111
+promedioK=0.0
+
+#Processing
+promedioK=(t_participacion_controladora2009+t_participacion_controladora2010+t_participacion_controladora2011)/3
+
+#Output
+print("Promedio de total de la participacion controladora: ",promedioK)
+
+#Programa25- Calcular el promedio de ventas netas
+#Declaración
+ventas_netas2005=0.0
+ventas_netas2008=0.0
+ventas_netas2014=0.0
+promedioR=0.0
+
+#Input
+ventas_netas2005=15.321
+ventas_netas2008=20.131
+ventas_netas2014=15.708
+promedioR=0.0
+
+#Processing
+promedioR=(ventas_netas2005+ventas_netas2008+ventas_netas2014)/3
+
+#Output
+print("Promedio de ventas netas: ",promedioR)
+
+#Programa26- Calcular el promedio de utilidad bruta
+#Declaración
+utilidad_bruta2005=0.0
+utilidad_bruta2006=0.0
+utilidad_bruta2007=0.0
+promedioG=0.0
+
+#Input
+utilidad_bruta2005=6.050
+utilidad_bruta2006=6.600
+utilidad_bruta2007=7.025
+promedioG=0.0
+
+#Processing
+promedioG=(utilidad_bruta2005+utilidad_bruta2006+utilidad_bruta2007)/3
+
+#Output
+print("Promedio de utilidad bruta: ",promedioG)
+
+#Programa27- Calcular el promedio total de capital contable
+#Declaración
+t_cap_contable2014=0.0
+t_cap_contable2013=0.0
+t_cap_contable2012=0.0
+promedioD=0.0
+
+#Input
+t_cap_contable2014=10.052
+t_cap_contable2013=11.366
+t_cap_contable2012=12.111
+promedioD=0.0
+
+
+#Processing
+promedioD=(t_cap_contable2014+t_cap_contable2013+t_cap_contable2012)/3
+
+#Output
+print("Promedio total de capital contable: ",promedioD)
+
+#Programa28- Calcular el total activos
+#Declaración
+t_activos2014=0.0
+t_activos2013=0.0
+t_activos2012=0.0
+total=0.0
+
+#Input
+t_activos2014=34.936
+t_activos2013=38.018
+t_activos2012=37.260
+Total=0.0
+
+#Processing
+Total=(t_activos2014+t_activos2013+t_activos2012)
+
+#Output
+print("Total: ",Total)
+
+#Programa29- Calcular el promedio de total pasivo
+#Declaración
+t_pasivo2005=0.0
+t_pasivo2006=0.0
+t_pasivo2007=0.0
+promedioX=0.0
+
+#Input
+t_pasivo2005=16.409
+t_pasivo2006=15.193
+t_pasivo2007=30.967
+promedioX=0.0
+
+#Processing
+promedioX=(t_pasivo2005+t_pasivo2006+t_pasivo2007)/3
+
+#Output
+print("Promedio de total pasivo: ",promedioX)
+
+#Programa30- Calcular el promedio de total de la participacion controladora
+#Declaración
+t_participacion_controladora2005=0.0
+t_participacion_controladora2006=0.0
+t_participacion_controladora2007=0.0
+promedioK=0.0
+
+#Input
+t_participacion_controladora2005=9.825
+t_participacion_controladora2006=12.859
+t_participacion_controladora2007=14.942
+promedioK=0.0
+
+#Processing
+promedioK=(t_participacion_controladora2005+t_participacion_controladora2006+t_participacion_controladora2007)/3
+
+#Output
+print("Promedio de total de la participacion controladora: ",promedioK)
+
+#Programa31- Calcular el promedio total de capital contable
+#Declaración
+t_cap_contable2005=0.0
+t_cap_contable2006=0.0
+t_cap_contable2007=0.0
+promedioZ=0.0
+
+#Input
+t_cap_contable2005=10.354
+t_cap_contable2006=14.779
+t_cap_contable2007=18.695
+promedioZ=0.0
+
+
+#Processing
+promedioZ=(t_cap_contable2005+t_cap_contable2006+t_cap_contable2007)/3
+
+#Output
+print("Promedio total de capital contable: ",promedioZ)
+
+#Programa32- Calcular el promedio de total pasivo
+#Declaración
+t_pasivo2008=0.0
+t_pasivo2010=0.0
+t_pasivo2012=0.0
+promedioN=0.0
+
+#Input
+t_pasivo2008=28.119
+t_pasivo2010=26.027
+t_pasivo2012=25.149
+promedioN=0.0
+
+#Processing
+promedioN=(t_pasivo2008+t_pasivo2010+t_pasivo2012)/3
+
+#Output
+print("Promedio de total pasivo: ",promedioN)
+
+#Programa33- Calcular el promedio de total de la participacion controladora
+#Declaración
+t_participacion_controladora2014=0.0
+t_participacion_controladora2013=0.0
+t_participacion_controladora2012=0.0
+promedioM=0.0
+
+#Input
+t_participacion_controladora2014=8.894
+t_participacion_controladora2013=10.221
+t_participacion_controladora2012=10.984
+promedioM=0.0
+
+#Processing
+promedioM=(t_participacion_controladora2014+t_participacion_controladora2013+t_participacion_controladora2012)/3
+
+#Output
+print("Promedio de total de la participacion controladora: ",promedioM)
+
+#Programa34- Calcular el total activos
+#Declaración
+t_activos2007=0.0
+t_activos2008=0.0
+t_activos2009=0.0
+total=0.0
+
+#Input
+t_activos2007=49.662
+t_activos2008=45.387
+t_activos2009=44.483
+Total=0.0
+
+#Processing
+Total=(t_activos2007+t_activos2008+t_activos2009)
+
+#Output
+print("Total: ",Total)
+
+#Programa35- Calcular el promedio total de capital contable
+#Declaración
+t_cap_contable2006=0.0
+t_cap_contable2008=0.0
+t_cap_contable2010=0.0
+promedioP=0.0
+
+#Input
+t_cap_contable2006=14.779
+t_cap_contable2008=17.268
+t_cap_contable2010=14.821
+promedioP=0.0
+
+
+#Processing
+promedioP=(t_cap_contable2006+t_cap_contable2008+t_cap_contable2010)/3
+
+#Output
+print("Promedio total de capital contable: ",promedioP)
+
+#Programa36- Calcular el promedio de total de la participacion controladora
+#Declaración
+t_participacion_controladora2005=0.0
+t_participacion_controladora2007=0.0
+t_participacion_controladora2009=0.0
+promedioY=0.0
+
+#Input
+t_participacion_controladora2005=9.825
+t_participacion_controladora2007=14.942
+t_participacion_controladora2009=16.339
+promedioY=0.0
+
+#Processing
+promedioY=(t_participacion_controladora2005+t_participacion_controladora2007+t_participacion_controladora2009)/3
+
+#Output
+print("Promedio de total de la participacion controladora: ",promedioY)
+
+#Programa37- Calcular el total activos
+#Declaración
+t_activos2010=0.0
+t_activos2012=0.0
+t_activos2014=0.0
+total=0.0
+
+#Input
+t_activos2010=40.848
+t_activos2012=37.260
+t_activos2014=34.936
+Total=0.0
+
+#Processing
+Total=(t_activos2010+t_activos2012+t_activos2014)
+
+#Output
+print("Total: ",Total)
+
+#Programa38- Calcular el promedio de total pasivo
+#Declaración
+t_pasivo2005=0.0
+t_pasivo2008=0.0
+t_pasivo2014=0.0
+promedioR=0.0
+
+#Input
+t_pasivo2005=28.119
+t_pasivo2008=26.027
+t_pasivo2014=25.149
+promedioR=0.0
+
+#Processing
+promedioR=(t_pasivo2005+t_pasivo2008+t_pasivo2014)/3
+
+#Output
+print("Promedio de total pasivo: ",promedioR)
+
+#Programa39- Calcular el promedio de ventas netas
+#Declaración
+ventas_netas2005=0.0
+ventas_netas2008=0.0
+ventas_netas2014=0.0
+promedioR=0.0
+
+#Input
+ventas_netas2005=15.321
+ventas_netas2008=20.131
+ventas_netas2014=15.708
+promedioR=0.0
+
+#Processing
+promedioR=(ventas_netas2005+ventas_netas2008+ventas_netas2014)/3
+
+#Output
+print("Promedio de ventas netas: ",promedioR)
+
+#Programa40- Calcular el promedio de gastos financieros
+#Declaración
+gastos_financieros2005=0.0
+gastos_financieros2008=0.0
+gastos_financieros2014=0.0
+promedioA=0.0
+
+#Input
+gastos_financieros2005=526.0
+gastos_financieros2008=910.0
+gastos_financieros2014=1.608
+promedioA=0.0
+
+#Processing
+promedioA=(gastos_financieros2005+gastos_financieros2008+gastos_financieros2014)/3
+
+#Output
+print("Promedio de gastos financieros: ",promedioR)
+
